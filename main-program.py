5cd9419a7fe5c213477f72b133703b4f52b59e75
Browarox <1996beryl@gmail.com>
2020-07-06 14:39:18 +0200

    added main program

--- main-program.py
+++ main-program.py
@@ -1,1 +1,9 @@
-print('Hello')
+import program2, MakeCSV, LoadDemand as Ld
+
+
+if input('Aby stworzyć nowy plik CSV wpisz N.\nAby wczytać dane z gotowego pliku Demand.csv wciśnij ENTER ') == 'N':
+    MakeCSV.start()
+else:
+    Ld.load()
+
+program2.start()
